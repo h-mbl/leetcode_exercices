4eadf5ad14d35fb533409cad3c479d193521c13f
herve.ngisse <125308992+h-mbl@users.noreply.github.com>
2024-05-03 14:41:23 -0400

    03/05 update

--- 165_compare versions number/script.py
+++ 165_compare versions number/script.py
@@ -1,25 +1,19 @@
-def compareVersions(a, b):
-    a = a.strip()
-    b = b.strip()
-    if len(a) >= 1 and len(b) >= 1 :
-        version1 = a.split(".")
-        version2 = b.split(".")
-        # egaliser les lens
-        if len(version1) < len(version2):
-            version1 = version1 + ["0"] * (len(version2) - len(version1))
-        elif len(version1) > len(version2):
-            version2 = version2 + ["0"] * (len(version1) - len(version2))
-        for i in range(len(version1)) :
-            if int(version1[i]) != int(version2[i]) :
-                if int(version1[i]) < int(version2[i]) :
-                    print("-1")
+def compareVersions(version1, version2):
+    version1 = list(map(int, version1.split('.')))
+    version2 = list(map(int, version2.split('.')))
+    max_length = max(len(version1), len(version2))
+    min_length = min(len(version1), len(version2))
+    version1.extend([0] * (max_length - len(version1)))
+    version2.extend([0] * (max_length - len(version2)))
+    if min_length >= 1:
+        for i in range(max_length):
+            tmp1 = version1[i]
+            tmp2 = version2[i]
+            if tmp1 != tmp2:
+                if tmp1 < tmp2:
                     return -1
-                elif int(version1[i]) > int(version2[i]) :
-                    print("1")
+                elif tmp1 > tmp2:
                     return 1
-            else :
-                continue
-        print("0")
         return 0
 
 compareVersions("0.1", "1.0")
