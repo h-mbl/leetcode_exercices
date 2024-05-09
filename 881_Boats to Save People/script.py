ce57a2c503cf74f83c13c3fa9594a5e038f04091
herve.ngisse <125308992+h-mbl@users.noreply.github.com>
2024-05-09 12:05:47 -0400

    Update script.py

--- 881_Boats to Save People/script.py
+++ 881_Boats to Save People/script.py
@@ -1,40 +1,71 @@
+from collections import defaultdict
 def boatsToSave(people,limit):
-    people = sorted(people)
-    i = 0
+    ensPeople = set(people)
+    combinaison = defaultdict(lambda: set())
+    occurrences = {}
     longueur = len(people)
-    j = -1
+    resultat = []
     nbBoats = 0
-    boat = {}
-    if 1 <= longueur <= 5 * 10**4 :
-        while len(people) > 0 and i < longueur:
-            tmp = people[j]
-            if 1 <= tmp <= limit <= 3 * 10**4:
-                reste = limit - tmp
-                if reste > 0  :
-                    found = False
-                    for k in range(len(people)-2, -1, -1):
-                        tmp2 = people[k]
-                        if tmp + tmp2 <= limit:
-                            boat[nbBoats] = (tmp, tmp2)
-                            people.pop(j)
-                            people.pop(k)
-                            nbBoats += 1
-                            found = True
-                            break
-                    if found == False:
-                        boat[nbBoats] = tmp
-                        people.pop(j)
+    if 1 >= longueur >= 5 * 10 ** 4 : return nbBoats
+
+    for element in people:
+        if element in occurrences:
+            occurrences[element] += 1
+        else:
+            occurrences[element] = 1
+
+    tmp = list(ensPeople)
+
+    for i in range(len(tmp)):
+        tmpi = tmp[i]
+        if 1<= tmpi <= limit <= 3 * 10**4 :combinaison[tmpi].add(tmpi)
+        if tmpi <= limit : combinaison[tmpi].add(tmpi)
+        for j in range(i+1,len(tmp)):
+            tmpj = tmp[j]
+            if tmpi+tmpj <= limit :
+                combinaison[tmpi].add(tmpj)
+                combinaison[tmpj].add(tmpi)
+
+    tmp = sorted(tmp)
+    k = -1
+    while len(tmp) > 0:
+        current = tmp[k]
+        if occurrences[current] == 0 :
+            tmp.pop()
+            continue
+        tmpens = combinaison[current]
+        longueur = len(tmpens)
+        tmpcompteur = set()
+        while len(combinaison[current]) != 0 and len(tmpcompteur) < longueur:
+            minimum = min(tmpens)
+            if occurrences[minimum] > 0 :
+                if current == minimum :
+                    a = 0
+                    if current + minimum <= limit :
+                        occurrences[minimum] -= 1
+                        occurrences[current] -= 1
+                        resultat.append((current, minimum))
+                        nbBoats += 1
+                    elif  current <= limit :
+                        occurrences[minimum] -= 1
+                        resultat.append((current))
                         nbBoats += 1
 
+                    else :
+                        a = 0
+                        tmpens.remove(minimum)
                 else :
-                    boat[nbBoats] = tmp
-                    people.pop(j)
+                    occurrences[minimum] -= 1
+                    occurrences[current] -= 1
+                    resultat.append((current,minimum))
                     nbBoats += 1
-            # le cas ou il y a une personne depassant la limite
-            else : return 0
-            i += 1
-        print(boat)
-        print(len(boat))
-        return len(boat)
-    else : return 0
-boatsToSave([3,5,3,4],5)
+                if occurrences[minimum] == 0 :
+                    combinaison[current].remove(minimum)
+                    tmpcompteur.add(minimum)
+            else : tmpcompteur.add(minimum)
+        tmp.pop()
+    print(resultat)
+    print(nbBoats)
+    return nbBoats
+
+boatsToSave([5,1,4,2],6)
